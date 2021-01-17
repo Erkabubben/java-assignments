d8a1c06c9afcc7c09553fe9a8a13e84325f6231b
Erik Lindholm <elimk06@student.lnu.se>
2021-01-18 00:18:02 +0100

    Add DangerousWork.java

--- src/Assignment1/DangerousWork.java
+++ src/Assignment1/DangerousWork.java
@@ -1,7 +1,35 @@
+/**
+ * 
+ */
 package Assignment1;
+import java.util.Scanner;
 
+/**
+ * @author Erik Lindholm
+ *
+ */
 public class DangerousWork {
+    
+    /**
+	 * @param args
+	 */
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How much would you like to earn? ");
+        int endSalary = in.nextInt();
         
+        double salary = 0.01;
+        int days = 0;
+
+        while (salary < endSalary && days < 30) {
+            salary = salary * 2;
+            days++;
+        }
+
+        System.out.println("You will have your money in " + days + " days.");
+
+        in.close();
     }
 }
